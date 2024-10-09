a4cb48e5f40bce79ac6e79f9f3c59ea8aa841d0e
M.Jasur <62153427+JASUR2007@users.noreply.github.com>
2024-10-09 22:01:16 +0500

    Add files via upload

---
+++ 4lesson/higher.py
@@ -0,0 +1,6 @@
+first = int(input("Write integer: "))
+second = int(input("Write integer: "))
+third = int(input("Write integer: "))
+num_list = [first, second, third]
+biggest_number = max(num_list)
+print(  biggest_number)
