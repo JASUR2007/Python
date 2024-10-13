60ffd9b9b5572631579fee3d46a796367c854840
M.Jasur <62153427+JASUR2007@users.noreply.github.com>
2024-10-13 23:32:46 +0500

    Add files via upload

---
+++ 6lesson/odd_elem.py
@@ -0,0 +1,10 @@
+# Task 6: Return Every Nth Element
+# Write a function that returns every nth element from a list.
+# Example:
+# lst = [10, 20, 30, 40, 50, 60, 70]
+# print(every_nth_element(lst, 2))  # Output: [20, 40, 60]
+
+
+
+lst = [10, 20, 30, 40, 50, 60, 70]
+print(lst[2 - 1:: 2])
