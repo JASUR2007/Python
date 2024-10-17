0ae3cefb345bc5e81e175338f3f6e82ffe3bbc88
M.Jasur <62153427+JASUR2007@users.noreply.github.com>
2024-10-17 23:46:03 +0500

    Add files via upload

---
+++ 9lesson/task1.py
@@ -0,0 +1,21 @@
+names = ('Father', 'Mother', 'Me', 'Little Brother')
+# rost = ('dog', 'cat')
+# names += rost
+# print(names)
+
+
+x = list(names)
+x.append('Dog')
+x.append('Cat')
+y = tuple(x)
+print(y)
+
+# print(names[0], names[1], names[2], names[3])
+
+# for i in names:
+#     print(f"Hello {i}")
+
+# s = 'Laptop'
+# n = 123
+# d = list(n)
+# print(d
